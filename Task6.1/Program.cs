d52b9d74fcfb1bca64f96f76501650b440e9c1f3
Maksim <maksimus.tumannick@yandex.ru>
2022-07-04 18:19:05 +0300

    Task6.1

---
+++ Task6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Max", 20, 321, 99);
+            player.ShowInfo();
+        }
+    }
+}
+ 
