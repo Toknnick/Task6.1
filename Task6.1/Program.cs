523d33f6d78d44120e71f1c65221c762e247ee07
Maksim <maksimus.tumannick@yandex.ru>
2022-07-04 20:24:25 +0300

    Task6.1

--- Task6.1/Program.cs
+++ Task6.1/Program.cs
@@ -14,5 +14,26 @@ namespace Task6._1
             player.ShowInfo();
         }
     }
+
+    class Player
+    {
+        private string _name;
+        private int _level;
+        private int _points;
+        private int _health;
+        
+        public Player(string name, int level, int points, int health)
+        {
+            _name = name;
+            _level = level;
+            _points = points;
+            _health = health;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {_name}. Уровень: {_level}. Очки: {_points}. Здоровье: {_health}");
+        }
+    }
 }
  
